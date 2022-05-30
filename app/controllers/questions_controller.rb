2517749c5241b2028266379e17882863ed879351
Tojo Rakotoarison <tojolalaina@gmail.com>
2022-05-30 14:33:15 +0200

    rails stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,10 +4,10 @@ class QuestionsController < ApplicationController
 
   def answer
     @ask = params[:ask]
-    if @ask == 'i am going to work right now!'
-      @answer = 'Great!'
-    elsif @ask.end_with?("?")
-      @answer = "Silly question, get dressed and go to work!"
+    if @ask == "i am going to work"
+      @answer = "Great!"
+    elsif @ask.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = "I don't care, get dressed and go to work!"
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'questions#home'
   get 'ask', to: 'questions#ask'
   get 'answer', to: 'questions#answer'
 
